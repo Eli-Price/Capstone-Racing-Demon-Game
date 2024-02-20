5c99160eae86cbe118eb741e3fc310134803a28f
Eli-Price <ewillp66@gmail.com>
2024-02-19 22:39:30 -0600

    Draggable new sprites, new Card class

--- src/card.ts
+++ src/card.ts
@@ -1,9 +1,24 @@
 import Phaser from 'phaser';
 
-export default interface Card {
+export default class Card {
+   name: string;
    suit: number;
    value: number;
    faceUp: boolean;
 
-   gameObject: Phaser.GameObjects.Image;
+   faceUpObject: Phaser.GameObjects.Sprite;
+   faceDownObject: Phaser.GameObjects.Sprite;
+   container: Phaser.GameObjects.Container;
+
+   constructor(name: string, suit: number, value: number, faceUp: boolean, faceUpObject: Phaser.GameObjects.Sprite, 
+                  faceDownObject: Phaser.GameObjects.Sprite, container: Phaser.GameObjects.Container) 
+   {
+      this.name = name;
+      this.suit = suit;
+      this.value = value;
+      this.faceUp = faceUp;
+      this.faceUpObject = faceUpObject;
+      this.faceDownObject = faceDownObject;
+      this.container = container;
+   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,91 @@
-import Phaser from 'phaser';
-import * as CardConfig from './card_config';
+import Phaser/*, { GameObjects }*/ from 'phaser';
+import Card from './card';
 import { Deck } from './deck';
 
-let card;
+//let card: Phaser.GameObjects.Sprite;
+//let cardS0: Card;
 
 class PlaygroundScene extends Phaser.Scene {
+   private mousePositionText!: Phaser.GameObjects.Text;
    constructor() {
       super('playground');
+      this.mousePositionText = {} as Phaser.GameObjects.Text;
    }
 
    preload() {
-      this.load.bitmapFont(
-         CardConfig.Font.NAME,
-         CardConfig.Font.IMAGE,
-         CardConfig.Font.CONFIG
-      );
-
       this.load.spritesheet(
-         'cards',
+         'cardsSpades',
          './assets/Top-Down/Cards/Spades-88x124.png',
          { frameWidth: 88, frameHeight: 124 }
+      );
+      this.load.spritesheet(
+         'cardsClubs',
+         './assets/Top-Down/Cards/Clubs-88x124.png',
+         { frameWidth: 88, frameHeight: 124 }
+      );
+      this.load.spritesheet(
+         'cardsDiamonds',
+         './assets/Top-Down/Cards/Diamonds-88x124.png',
+         { frameWidth: 88, frameHeight: 124 }
+      );
+      this.load.spritesheet(
+         'cardsHearts',
+         './assets/Top-Down/Cards/Hearts-88x124.png',
+         { frameWidth: 88, frameHeight: 124 }
+      );
+      this.load.spritesheet(
+         'cardBacks',
+         './assets/Top-Down/Cards/Card_Back-88x124.png',
+         { frameWidth: 88, frameHeight: 124 }
+      );
+      this.load.spritesheet(
+         'cardsDecks',
+         './assets/Top-Down/Cards/Card_DeckA-88x140.png',
+         { frameWidth: 88, frameHeight: 140 }
       );
    }
 
    create() {
       const deck = new Deck(this);
-      deck.Deal();
+      //deck.Shuffle();
 
-      card = this.add.sprite(88, 124, 'cards', 0);
+      // Deck testing code
+      console.log(deck.cards);
+      let card17 = this.add.existing(deck.cards[17].container);
+      card17.setInteractive(new Phaser.Geom.Rectangle(-44, -62, 88, 124), Phaser.Geom.Rectangle.Contains);
+      this.input.setDraggable(card17);
+      card17.x = 88;
+      card17.y = 224;
+
+      this.cameras.main.setBackgroundColor('#408080');
+
+      //const card = this.add.sprite(88, 224, 'cardsDecks', 1);
+      //card.setInteractive({ draggable: true });
+      //this.input.setDraggable(card);
+
+      let card12 = this.add.existing(deck.cards[12].container);
+      deck.flipCard(deck.cards[12], true);
+      card12.setInteractive(new Phaser.Geom.Rectangle(-44, -62, 88, 124), Phaser.Geom.Rectangle.Contains);
+      this.input.setDraggable(card12);
+      card12.x = 248;
+      card12.y = 224;
+
+      this.input.on('pointerdown', (_pointer: PointerEvent, container: Phaser.GameObjects.Container) => {
+         //this.children.bringToTop(card.gameObject);
+         console.log(container);
+         if (container) {
+            //deck.flipCard(container, !(container.faceUp));
+         }
+      });
+
+      this.mousePositionText = this.add.text(10, 10, '', { color: '#ffffff' });
+
+      this.input.on('drag', (_pointer: PointerEvent, parentContainer: Phaser.GameObjects.Container, 
+                                dragX: number, dragY: number) => {
+            parentContainer.x = dragX;
+            parentContainer.y = dragY;
+         }
+      );
    }
 
    update() {
@@ -35,18 +93,7 @@ class PlaygroundScene extends Phaser.Scene {
       var mouseY = this.input.mousePointer.y;
       mouseX = mouseX | 0;
       mouseY = mouseY | 0;
-      var positionText = `Mouse Position: (${mouseX}, ${mouseY})`;
-
-      this.children.each((child) => {
-         if (
-            child instanceof Phaser.GameObjects.Text &&
-            child.text.startsWith('Mouse Position')
-         ) {
-            child.destroy();
-         }
-      });
-
-      this.add.text(10, 10, positionText, { color: '#ffffff' });
+      this.mousePositionText.setText(`Mouse Position: (${mouseX}, ${mouseY})`);
    }
 }
 
@@ -56,7 +103,7 @@ const config: Phaser.Types.Core.GameConfig = {
    scale: {
       parent: 'game',
       width: 960,
-      height: 540,
+      height: 640,
       mode: Phaser.Scale.NONE,
       autoCenter: Phaser.Scale.CENTER_BOTH,
    },
